502377378f442e63d6d26b4ca463265143e9ea98
hareeshcv <chareesh8519@gmail.com>
2025-05-01 18:49:46 +0530

    Java8 Sample realtime examples

---
+++ src/com/employee/code/AverageSalary.java
@@ -0,0 +1,29 @@
+package com.employee.code;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class AverageSalary {
+    public static void main(String[] args){
+        List<Employee> employeeList = EmployeeList.getEmployeeList();
+        //What is the average salary of each department?
+        Map<String, Double> avgSalaryOfDepart = employeeList.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment,Collectors.averagingDouble(Employee::getSalary)));
+        Set<Map.Entry<String,Double>> entrySet = avgSalaryOfDepart.entrySet();
+        for(Map.Entry<String,Double> entry : entrySet){
+            System.out.println(entry.getKey() +":"+ entry.getValue());
+        }
+        //Get the details of youngest male employee in the product development department?
+        Optional<Employee> youngestEmployeeProductDevelopment =
+                employeeList.stream().filter(e->e.getGender()=="Male" && e.getDepartment()=="Product Development").
+                        min(Comparator.comparingInt(Employee::getAge));
+        Employee youngestEmployee = youngestEmployeeProductDevelopment.get();
+        System.out.println("Get the All List of Youngest Employees for product development....");
+        System.out.println("Employee Id: " + youngestEmployee.getId());
+        System.out.println("Name: "+ youngestEmployee.getName());
+        System.out.println("Gender : " + youngestEmployee.getGender());
+        System.out.println("Department :"+ youngestEmployee.getDepartment());
+        System.out.println("YearOfJoining : " + youngestEmployee.getYearOfJoining());
+        System.out.println("Salary : "+ youngestEmployee.getSalary());
+    }
+}
